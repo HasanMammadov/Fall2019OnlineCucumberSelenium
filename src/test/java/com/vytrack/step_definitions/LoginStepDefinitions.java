272e559f374ea4527c73d6f7bbd0bc97817eed45
hasan <hasanmamedoff@gmail.com>
2020-04-11 21:14:24 -0400

    definition claass added

--- src/test/java/com/vytrack/step_definitions/LoginStepDefinitions.java
+++ src/test/java/com/vytrack/step_definitions/LoginStepDefinitions.java
@@ -1,28 +1,44 @@
 package com.vytrack.step_definitions;
 
+import com.vytrack.pages.LoginPage;
+import com.vytrack.utilities.ConfigurationReader;
+import com.vytrack.utilities.Driver;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
 
 public class LoginStepDefinitions {
+    LoginPage loginPage = new LoginPage();
 
     @Given("user is on the login page")
     public void user_is_on_the_login_page() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        System.out.println("Open login page");
+        String url = ConfigurationReader.getProperty("qa3");
+        Driver.getDriver().get(url);
+        Driver.getDriver().manage().window().maximize();
     }
 
     @When("user logs in as a sales manager")
     public void user_logs_in_as_a_sales_manager() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        System.out.println("Login as sales manager");
+        loginPage.login("storemanager85","UserUser123");
     }
 
     @Then("user should verify that title is a Dashboard")
     public void user_should_verify_that_title_is_a_Dashboard() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        System.out.println("verify title is Dashboard");
+        Assert.assertEquals("Dashboard",Driver.getDriver().getTitle());
+        Driver.closeDriver();
     }
+
+
+    @When("user logs in as a store manager")
+    public void user_logs_in_as_a_store_manager() {
+       loginPage.login("storemanager85","UserUser123");
+    }
+
+
 
 
 
